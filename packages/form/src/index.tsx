61715f1b9318e28c79628bc5076af39ee0ee664c
yll <yangleilei@pwrd.com>
2022-03-23 16:17:28 +0800

    ✨ feat(packages): Form 组件

---
+++ packages/form/src/index.tsx
@@ -0,0 +1,10 @@
+import React, { PureComponent } from 'react'
+import Button from "@mono-repos/button";
+
+const Form = (props) => {
+    return <div>
+        <Button>save</Button>
+    </div>
+}
+
+export default Form
