42af2bbfd16f030643cd07626ae47d242150348d
yll <837865811@qq.com>
2022-07-20 22:50:22 +0800

    ✨ feat: 测试

--- packages/form/src/index.tsx
+++ packages/form/src/index.tsx
@@ -1,10 +1,18 @@
+/*
+ * @Author: yll 837865811@qq.com
+ * @Date: 2022-07-20 21:35:53
+ * @LastEditors: yll 837865811@qq.com
+ * @LastEditTime: 2022-07-20 22:50:01
+ * @FilePath: /mono-repos/packages/form/src/index.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import React, { PureComponent } from 'react'
 import Button from "@wuaixiaoyao-lib/button";
 
 const Form = (props) => {
     return <div>
         form
-        <Button>点击保存</Button>
+        <Button>点击保存 测试点击</Button>
     </div>
 }
 
--- packages/utils/src/index.ts
+++
@@ -1,6 +0,0 @@
-import isImg from "./isImg";
-import isNil from "./isNil";
-import isUrl from "./isUrl";
-import { merge } from "./merge";
-
-export { isImg, isUrl, isNil, merge };
