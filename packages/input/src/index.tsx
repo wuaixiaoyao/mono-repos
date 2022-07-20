c7eeb8ba82bc955a0ad324fdfeba410e2778695e
yll <837865811@qq.com>
2022-07-20 23:01:19 +0800

    ✨ feat: input 修改

--- packages/input/src/index.tsx
+++ packages/input/src/index.tsx
@@ -1,9 +1,17 @@
+/*
+ * @Author: yll 837865811@qq.com
+ * @Date: 2022-07-20 21:35:53
+ * @LastEditors: yll 837865811@qq.com
+ * @LastEditTime: 2022-07-20 23:00:57
+ * @FilePath: /mono-repos/packages/input/src/index.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import React from "react";
 import styled from 'styled-components';
 
 const Input = styled.input`
     border: 1px solid #ccc;
-    border-radius: 4px;
+    border-radius: 6px;
     box-sizing: border-box;
     font-size: 16px;
     font-weight: 300;
