0a5254c341e54d2d18302b0a76965a8ee22937d4
yangleilei <yangleilei@pwrd.com>
2022-08-15 15:00:17 +0800

    ✨ feat(Input): UI修改

--- packages/input/src/index.tsx
+++ packages/input/src/index.tsx
@@ -7,16 +7,16 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Input = styled.input`
-    border: 1px solid #ccc;
-    border-radius: 6px;
-    box-sizing: border-box;
-    font-size: 16px;
-    font-weight: 300;
-    padding: 10px 40px 10px 10px;
-    width: 150px;
+  border: 1px solid #ccc;
+  border-radius: 6px;
+  box-sizing: border-box;
+  font-size: 16px;
+  font-weight: 300;
+  padding: 10px 40px 10px 10px;
+  width: 250px;
 `;
 
 export default Input;
